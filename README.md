04a71b475a200a32f12073c577b97ac66ab1eeda
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2024-02-22 02:32:51 +0300

    init hw

---
+++ README.md
@@ -0,0 +1,13 @@
+#Array Methods Homework
+
+Introduction
+
+you have array of ages=[25, 18, 12, 16, 40] , you are requested the following :
+
+Use a for loop to iterate over the ages array and double each age. Store the doubled ages in a new array called doubledAges and Print the doubledAges array to the console.
+
+Use the forEach method to loop through the ages array and print each age to the console.
+
+Use the filter method to create a new array called adults that contains only ages greater than or equal to 18.
+
+Use the map method to create a new array called ageStrings that contains string representations of each age in the ages array.
---
+++ index.js
@@ -0,0 +1,16 @@
+function processAges(ages) {
+  //!your solution should be inside the function.
+  //1-Use a for loop to iterate over the ages array and double each age. Store the doubled ages in a new array called doubledAges and Print the doubledAges array to the console.
+
+  //2-Use the forEach method to loop through the ages array and print each age to the console.
+
+  //3-Use the filter method to create a new array called adults that contains only ages greater than or equal to 18.
+
+  //4-Use the map method to create a new array called ageStrings that contains string representations of each age in the ages array.
+
+  //5-please make sure to name your arrays(variables) accordingly to the names in the return statement.
+
+  return { doubledAges, adults, ageStrings };
+}
+
+module.exports = processAges;
