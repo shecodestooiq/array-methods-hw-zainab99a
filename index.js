10e67ff975698d1555d96c44034871218288c2d5
zainab99a <amanhal729@gmail.com>
2024-02-23 20:13:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function processAges(ages) {
   //!your solution should be inside the function.
   let doubledAges=[];
-  let adults;
-  let ageStrings;
-  
   //1-Use a for loop to iterate over the ages array and double each age. Store the doubled ages in a new array called doubledAges and Print the doubledAges array to the console.
-  for (const age of ages) {
-    doubledAges.push(age*2);
+  for (let i=0; i<ages.length;i++) {
+    doubledAges.push(ages[i]*2);
   }
   console.log(doubledAges);
  
@@ -16,13 +13,13 @@ console.log(age);
 });
 
   //3-Use the filter method to create a new array called adults that contains only ages greater than or equal to 18.
-  adults=ages.filter(function(age){
+  let adults=ages.filter(function(age){
     return age>=18;
 });
 console.log(adults)
 
   //4-Use the map method to create a new array called ageStrings that contains string representations of each age in the ages array.
-  ageStrings = ages.map(function(age){
+  let ageStrings = ages.map(function(age){
     return age.toString();
 });
 console.log(ageStrings)
@@ -32,4 +29,4 @@ console.log(ageStrings)
   return { doubledAges, adults, ageStrings };
 }
 const ages=[25, 18, 12, 16, 40];
-module.exports = processAges(ages);
+module.exports = processAges;
